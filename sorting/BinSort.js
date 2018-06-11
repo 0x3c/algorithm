07ee72dfff35c774c2295a23603a1a2472098257
SamuelChia <funwithes6@gmail.com>
2018-06-11 22:38:29 +0800

    init

---
+++ sorting/BinSort.js
@@ -0,0 +1,28 @@
+const easy = require("../utils/index");
+
+class BinSort {
+  sort(originArr) {
+    const arr = [...originArr];
+    console.log(arr);
+
+    console.log(arr);
+  }
+}
+
+// new BinSort().sort([
+//   99,
+//   432,
+//   423,
+//   423,
+//   51,
+//   51,
+//   346,
+//   31,
+//   67,
+//   137,
+//   14,
+//   578,
+//   28,
+//   45,
+//   87
+// ]);
