ad6234737b519032e40e3d25eb66e0fc86ae2f2c
Samuel Chia <funwithes6@gmail.com>
2020-02-17 15:55:34 +0800

    Modified SelectionSort

--- sorting/SelectionSort.js
+++ sorting/SelectionSort.js
@@ -17,14 +17,15 @@ class SelectionSort {
     const arr = [...originArr];
 
     for (let i = 0; i < arr.length - 1; i++) {
-      let min = arr[i];
+      let minIdx = i;
       for (let j = i + 1; j < arr.length; j++) {
-        if (easy.lessThan(arr[j], arr[i])) {
-          const temp = arr[i];
-          arr[i] = arr[j];
-          arr[j] = temp;
+        if (easy.lessThan(arr[j], arr[minIdx])) {
+          minIdx = j;
         }
       }
+      const temp = arr[i];
+      arr[i] = arr[minIdx];
+      arr[minIdx] = temp;
     }
 
     return arr;
