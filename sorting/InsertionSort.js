c540b21d88b30d4e0bc3fdd1aaf986c9017b6813
Samuel Chia <funwithes6@gmail.com>
2020-02-17 16:49:12 +0800

    Add InsertionSort description

--- sorting/InsertionSort.js
+++ sorting/InsertionSort.js
@@ -1,15 +1,25 @@
 const easy = require("../utils/index");
 
+/**
+ * 数组分成两部分, 已排序数组和未排序数组, 每次从未排序数组中取出一个元素,
+ * 插入到已排序数组中正确的位置, 直到未排序数组为空.
+ *
+ * 稳定排序
+ * 时间复杂度: O(n) = O(n^2)
+ * 最好情况, 每次都插入到第一个位置 n-1 -> O(n)
+ *
+ */
 class InsertionSort {
   sort(originArr) {
     const arr = [...originArr];
 
-    for (let i = 0; i < arr.length - 1; i++) {
-      for (let j = 0; j <= i; j++) {
+    for (let i = 1; i < arr.length; i++) {
+      for (let j = 0; j < i; j++) {
         if (easy.lessThan(arr[i], arr[j])) {
           const temp = arr[i];
           arr.splice(i, 1);
           arr.splice(j, 0, temp);
+          break;
         }
       }
     }
